cf2c98df538cd81f4f9b022995d0af5c64754f15
mohamedvpn <103552750+mohamedvpn@users.noreply.github.com>
2024-11-08 02:15:33 +0200

    Update Activation_1.py

--- Activation_1.py
+++ Activation_1.py
@@ -1,3 +1,3 @@
 {
-  "6JJ8-DGGR-3ZD4-YTPL":{'name':'Mohamed Gamal','end_date':'11/16/2024'},
+  "6JJ8-DGGR-3ZD4-YTPL":{'name':'Mohamed Gamal','mac':'e4a3da37ff5f9b92d209509d19b5579b09347c1e4b401f10c5c7bc2c4c8dbc3eXYZ789'},
 }
