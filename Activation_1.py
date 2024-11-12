ca540a1b83227a9aa2e62d78f6c24d5e43da2486
mohamedvpn <103552750+mohamedvpn@users.noreply.github.com>
2024-11-12 14:27:17 +0200

    Update Activation_1.py

--- Activation_1.py
+++ Activation_1.py
@@ -1,5 +1,5 @@
 {
   "6JJ8-DGGR-3ZD4-YTPL":{'name':'Mohamed Gamal','mac':'e4a3da37ff5f9b92d209509d19b5579b09347c1e4b401f10c5c7bc2c4c8dbc3ed8d6S4n0'},
   "6JJ2H8-DGGRd-3ZD4s-Y4TP6L":{'name':'Nader Abo Bakr','mac':'f524636b48f4450635c4661cf2f1a19898f9ab4f91ef4db94d7d8fff442fbd0ad8d6S4n0'},
-  "6JJ2H38-D4GGRd-3ZD54s-Y46TP6L1":{'name':'Nader Abo Bakr','v241dd46584e543524b7ca1fcfef8b3f0481dac427c07ae47c8397bf0ac725da2d8d6S4n0'},
+  "6JJ2H38-D4GGRd-3ZD54s-Y46TP6L1":{'name':'Nader Abo Bakr','mac':'v241dd46584e543524b7ca1fcfef8b3f0481dac427c07ae47c8397bf0ac725da2d8d6S4n0'},
 }
