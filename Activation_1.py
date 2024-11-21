4508d0edebeb7f26afd5c4c6198aeb51123b2cd3
mohamedvpn <103552750+mohamedvpn@users.noreply.github.com>
2024-11-22 01:19:47 +0200

    Update Activation_1.py

--- Activation_1.py
+++ Activation_1.py
@@ -2,5 +2,7 @@
   "6JJ8-DGGR-3ZD4-YTPL":{'name':'Mohamed Gamal','mac':'e4a3da37ff5f9b92d209509d19b5579b09347c1e4b401f10c5c7bc2c4c8dbc3ed8d6S4n0'},
   "6JJ2H8-DGGRd-3ZD4s-Y4TP6L":{'name':'Nader Abo Bakr','mac':'f524636b48f4450635c4661cf2f1a19898f9ab4f91ef4db94d7d8fff442fbd0ad8d6S4n0'},
   "6JJ2H38-D4GGRd-3ZD54s-Y46TP6L1":{'name':'Commander Ali Abdel Salam','mac':'5c27e24d674f93672a357d250697a685cd5d5c1ba0912d0c7e35089685aa6127d8d6S4n0'},
-  "6JJ2rH38-D4GGrRd-3tZD54s-gdfg":{'name':'Radwan','mac':'4e4a6decc1a8c030e09903aff9276b1fb130e42b87349f787481d2ed7d07ef4bd8d6S4n0','date':'19-11-2024'}
+  "6JJ2rH38-D4GGrRd-3tZD54s-gdfg":{'name':'Radwan','mac':'4e4a6decc1a8c030e09903aff9276b1fb130e42b87349f787481d2ed7d07ef4bd8d6S4n0','date':'19-11-2024'},
+  "T-D4GGrRd-3tZD54SS":{'name':'Test','mac':'74bea2918a187148f677c2dd8761137bcd2f38c8dc0993da208c06514b470f11d8d6S4n0','date':'19-11-2024'},
+  
 }
