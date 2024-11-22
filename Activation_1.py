9f16e55c07669011ff3726e149279101aff53afb
mohamedvpn <103552750+mohamedvpn@users.noreply.github.com>
2024-11-22 23:32:42 +0200

    Update Activation_1.py

--- Activation_1.py
+++ Activation_1.py
@@ -3,5 +3,5 @@
   "6JJ2H8-DGGRd-3ZD4s-Y4TP6L":{'name':'Nader Abo Bakr','mac':'f524636b48f4450635c4661cf2f1a19898f9ab4f91ef4db94d7d8fff442fbd0ad8d6S4n0'},
   "6JJ2H38-D4GGRd-3ZD54s-Y46TP6L1":{'name':'Commander Ali Abdel Salam','mac':'5c27e24d674f93672a357d250697a685cd5d5c1ba0912d0c7e35089685aa6127d8d6S4n0'},
   "6JJ2rH38-D4GGrRd-3tZD54s-gdfg":{'name':'Radwan','mac':'4e4a6decc1a8c030e09903aff9276b1fb130e42b87349f787481d2ed7d07ef4bd8d6S4n0','date':'19-11-2024'},
-  "T-D24GG4rRd-3tZ2D54SS342":{'name':'Test','mac':'f6f289d2395f86ba6a62d10ecddd960cee67b5a34ac2061653d758c18ce0d1f8d8d6S4n0','date':'19-11-2024'}
+  "154T-D24GG554rRd-3tZ2D54SS342":{'name':'Test','mac':'f6f289d2395f86ba6a62d10ecddd960cee67b5a34ac2061653d758c18ce0d1f8d8d6S4n0','date':'19-11-2024'}
 }
